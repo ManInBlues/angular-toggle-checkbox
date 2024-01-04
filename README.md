77fd4dbce5232a581dc75c1c6bfb894e6ebaf752
ManInBlues <155654509+ManInBlues@users.noreply.github.com>
2024-01-04 11:11:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-toggle-checkbox
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ManInBlues/angular-toggle-checkbox)
---
+++ src/app/toggle-checkbox/toggle-checkbox.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'toggle-checkbox',
+  templateUrl: './toggle-checkbox.component.html',
+  styleUrls: ['./toggle-checkbox.component.scss'],
+})
+export class ToggleCheckBoxComponent {}
